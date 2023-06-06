d9547c60c9b40c94ecae55ef7ceda0039e18a4bf
FLY123 <akfakf0509@naver.com>
2023-06-06 23:34:51 +0900

    feat: 팀 컴포넌트 작성 및 적용

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Team from "~/components/Team.vue";
+import { useMatch } from "~/stores/match";
+
+const { value, addTeam } = useMatch();
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="(i, index) in value.team" :key="`team-${index}`">
+        <Team v-bind="i" />
+      </li>
+    </ul>
+
+    <button @click="addTeam()">Create Team</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
