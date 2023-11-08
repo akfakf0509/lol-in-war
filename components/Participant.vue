6ca62d0b2fa84a2be85d522cc23f1921075640e9
FLY123 <akfakf0509@naver.com>
2023-11-09 01:04:14 +0900

    feat: Participant Item and List component

---
+++ components/Participant.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  name: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <li>
+    {{ name }}
+  </li>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ components/ParticipantList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+interface Props {
+  participants: Participant[];
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <section class="participants">
+    <Header text="참가자 목록"></Header>
+    <ul class="participants__list">
+      <Participant
+        v-for="participant in participants"
+        :key="participant.name"
+        v-bind="participant"
+      />
+    </ul>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.participants {
+  &__title {
+    @include font-heading-l;
+  }
+  &__list {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 16px;
+
+    list-style: none;
+  }
+}
+</style>
