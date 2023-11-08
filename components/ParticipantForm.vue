85e166a2906164d0bc25573259f0e785eb4ea356
FLY123 <akfakf0509@naver.com>
2023-11-09 01:04:22 +0900

    feat: Participant form component

---
+++ components/ParticipantForm.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Emits {
+  (e: "submit", name: string): void;
+}
+const emit = defineEmits<Emits>();
+
+const name = ref("");
+
+function handleSubmit() {
+  if (!name.value) return;
+  emit("submit", name.value);
+  name.value = "";
+}
+</script>
+
+<template>
+  <form class="form" @submit.prevent="handleSubmit()">
+    <Header text="참가자 추가"></Header>
+    <input v-model="name" class="form__input" />
+    <button type="submit">추가</button>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+.form {
+  &__input {
+    margin-right: 8px;
+
+    background-color: $color-surface;
+    border: 1px solid $color-border;
+  }
+}
+</style>
