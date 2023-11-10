88821dd89335d46fbd331072c9182279c9c95560
FLY123 <woodongkyan0509@gmail.com>
2023-11-11 04:21:15 +0900

    feat: 생성된 팀 보여주는 컴포넌트

---
+++ components/Result.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface Props {
+  team1: Participant[];
+  team2: Participant[];
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <section class="result">
+    <Header text="MATCH UP" class="result__header"></Header>
+    <ul class="result__body">
+      <li>
+        <ResultList :team="team1" />
+      </li>
+      <p class="result__vs" v-text="'VS'"></p>
+      <li>
+        <ResultList :team="team2" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.result {
+  &__header {
+    margin-bottom: 64px;
+    text-align: center;
+  }
+  &__body {
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+
+    list-style: none;
+  }
+  &__vs {
+    @include font-heading-s;
+    text-align: center;
+  }
+}
+</style>
