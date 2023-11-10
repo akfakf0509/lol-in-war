929d28d510769f79b261ed8eab7add433d57c6e6
FLY123 <akfakf0509@naver.com>
2023-11-11 04:21:15 +0900

    feat: 생성된 팀 보여주는 컴포넌트

---
+++ components/ResultItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  name: string;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <li>
+    <p v-text="name" class="item-name"></p>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+.item-name {
+  @include font-heading-xs;
+}
+</style>
